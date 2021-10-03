c44d094b22bcdfe79261af229decba8022b5d1c3
nyancodeid <ryandevstudio@gmail.com>
2021-10-03 16:36:09 +0700

    update(component): app-hero

--- src/components/AppHero.vue
+++ src/components/AppHero.vue
@@ -2,10 +2,11 @@
 </script>
 
 <template>
-  <div class="py-24 flex flex-col items-center">
+  <div class="py-12 flex flex-col items-center">
     <h1 class="text-6xl font-bold mb-4 text-transparent bg-clip-text bg-gradient-to-br from-pink-400 to-purple-600">#Quote's</h1>
-    <p class="w-11/12 md:w-1/2 mb-4 text-sm md:text-xl text-center text-gray-500">Kumpulan quote di dunia sebagai penyemangat dan nasihat. Dibuat untuk memeraiahkan Event Hacktoberfest 2021.</p>
-    <div class="w-11/12 md:w-1/2 flex justify-center">
+    <p class="w-11/12 md:w-1/2 mb-4 text-sm md:text-xl text-center text-gray-500">Kumpulan quote oleh orang-orang hebat di dunia sebagai penasehat dan penyemangat. Dibuat untuk memeraiahkan event Hacktoberfest 2021.</p>
+    
+    <div class="w-11/12 md:w-1/2 flex justify-center mb-4">
       <a class="mr-1" href="https://github.com/nyancodeid/quotes">
         <img src="https://img.shields.io/badge/github-quotes-brightgreen?logo=github&style=flat" alt="Repository">
       </a>
