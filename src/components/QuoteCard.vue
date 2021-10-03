aa7191656b6afaecb65e377df6d0fb28fbcbf709
nyancodeid <ryandevstudio@gmail.com>
2021-10-03 18:38:05 +0700

    update(component): quote-card define props

--- src/components/QuoteCard.vue
+++ src/components/QuoteCard.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { defineProps, PropType, ref } from "vue";
+import { defineProps, toRef } from "vue";
 import { gradients } from "../utils/gradients"; 
 
 interface GithubProfile {
@@ -18,11 +18,11 @@ interface Quote {
   author: string
 }
 
-const props = defineProps({
-  quote: {
-    type: Object as PropType<Quote>
-  }
-});
+const props = defineProps<{
+  quote: Quote
+}>();
+
+const quote = toRef(props, "quote");
 
 function getGradientByIndex (index: number = 0) {
   return gradients[index];
@@ -31,26 +31,26 @@ function getGradientByIndex (index: number = 0) {
 
 <template>
   <div class="w-full relative text-white overflow-hidden rounded-3xl flex shadow-lg">
-    <div data-id="test" class="w-full flex flex-col" :class="getGradientByIndex(props.quote?.gradient_id)">
+    <div data-id="test" class="w-full flex flex-col" :class="getGradientByIndex(quote.gradient_id)">
       <div class="sm:max-w-sm sm:flex-none md:w-auto flex flex-col items-start relative p-6 xl:p-8">
         <h2 class="text-xl font-semibold mb-2">
           <i-ri-double-quotes-l />
         </h2>
         <p class="font-medium text-lg text-white mb-4">
-          {{ props.quote?.text }}
+          {{ quote.text }}
         </p>
-        <p class="text-sm text-gray-100">- {{ props.quote?.author }}</p>
+        <p class="text-sm text-gray-100">- {{ quote.author }}</p>
       </div>
       <div class="mt-auto p-6 pt-1">
-        <div class="flex items-start" v-if="props.quote?.github?.available">
-          <img :src="props.quote?.github?.avatar_url" class="rounded-full w-[24px] h-[24px] mr-2" loading="lazy" alt="Github Profile Pic" />
+        <div class="flex items-start" v-if="quote.github?.available">
+          <img :src="quote.github?.avatar_url" class="rounded-full w-[24px] h-[24px] mr-2" loading="lazy" alt="Github Profile Pic" />
 
           <div class="flex flex-col">
-            <a class="text-xs" :href="`https://github.com/${props.quote?.username}`" nofollow="true" target="_blank">{{ props.quote?.github?.name }}</a>
+            <a class="text-xs" :href="`https://github.com/${quote.username}`" nofollow="true" target="_blank">{{ quote.github?.name }}</a>
 
             <div class="flex items-center">
               <span class="text-xs mr-2 text-white text-opacity-80">
-                <span class="font-bold text-white">{{ props.quote?.github?.followers }}</span> followers
+                <span class="font-bold text-white">{{ quote.github?.followers }}</span> followers
               </span>
             </div>
           </div>
