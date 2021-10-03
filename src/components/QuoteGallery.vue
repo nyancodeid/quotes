206d39c4df33b23a960d2c991fb2e43b3aaab2e5
nyancodeid <ryandevstudio@gmail.com>
2021-10-03 17:09:41 +0700

    update(component): quote-gallery submit quote

--- src/components/QuoteGallery.vue
+++ src/components/QuoteGallery.vue
@@ -24,17 +24,17 @@ onMounted(async function() {
       <div class="grid grid-cols-1 md:grid-cols-3 gap-5 md:gap-6 xl:gap-8">
         <section class="flex">
           <div class="w-full relative text-white overflow-hidden rounded-3xl flex shadow-lg">
-            <div class="w-full flex flex-col bg-gray-200">
+            <div class="w-full flex flex-col bg-gradient-to-br from-red-100 to-blue-100">
               <div class="sm:max-w-sm sm:flex-none md:w-auto flex flex-col items-start relative p-6 xl:p-8">
-                <h1 class="text-xl font-semibold mb-2 text-gray-800">
-                  <i-ri-chat-quote-line />
+                <h1 class="mb-2 text-gray-800">
+                  <i-ri-chat-quote-line class="text-3xl" />
                 </h1>
-                <p class="font-medium text-lg text-gray-800 mb-4">
-                  Submit quote milimu dengan cara berkontribusi di github langsung.
+                <p class="font-medium text-2xl text-gray-800 mb-4">
+                  Submit quote milimu dengan berkontribusi langsung di Repository Github.
                 </p>
               </div>
               <div class="p-6 pt-1 mt-auto">
-                <a class="rounded-xl text-gray-800 bg-gray-300 hover:bg-[#afb8c5] transition-colors py-2 px-4" href="https://github.com/nyancodeid/quotes#contribute">Kontribusi Sekarang <i-ri-arrow-right-line class="inline-block" /></a>
+                <a class="rounded-xl text-gray-800 bg-gray-300 hover:bg-blue-200 transition-colors py-2 px-4" href="https://github.com/nyancodeid/quotes#contribute">Kontribusi Sekarang <i-ri-arrow-right-line class="inline-block" /></a>
               </div>
             </div>
           </div>
