04ff063f5262c4fd0492a6beca668f154af1f563
nyancodeid <ryandevstudio@gmail.com>
2021-10-03 17:09:19 +0700

    update(component): app-hero fix typo

--- src/components/AppHero.vue
+++ src/components/AppHero.vue
@@ -4,7 +4,7 @@
 <template>
   <div class="py-12 flex flex-col items-center">
     <h1 class="text-6xl font-bold mb-4 text-transparent bg-clip-text bg-gradient-to-br from-pink-400 to-purple-600">#Quote's</h1>
-    <p class="w-11/12 md:w-1/2 mb-4 text-sm md:text-xl text-center text-gray-500">Kumpulan quote oleh orang-orang hebat di dunia sebagai penasehat dan penyemangat. Dibuat untuk memeraiahkan event Hacktoberfest 2021.</p>
+    <p class="w-11/12 md:w-1/2 mb-4 text-sm md:text-xl text-center text-gray-500">Kumpulan quote oleh orang-orang hebat di dunia sebagai penasehat dan penyemangat. Dibuat untuk memeriahkan event Hacktoberfest 2021.</p>
     
     <div class="w-11/12 md:w-1/2 flex justify-center mb-4">
       <a class="mr-1" href="https://github.com/nyancodeid/quotes">
