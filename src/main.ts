b42f2738f75f6cda9b2b2962f681a0e8acb1b1e1
nyancodeid <ryandevstudio@gmail.com>
2021-10-05 01:03:43 +0700

    update(main): use vite-ssg

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
-import { createApp } from 'vue'
+import { ViteSSG } from 'vite-ssg/single-page'
+
 import App from './App.vue'
 
 import './styles/tailwind.css'
 
-createApp(App).mount('#app')
+export const createApp = ViteSSG(App)
