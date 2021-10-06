24331b43a0a1bea3ab36f429ab701dd2399f3173
Lord Fahdan <lordfahdan@gmail.com>
2021-10-06 17:31:08 +0700

    feat: add search feature

--- src/components/QuoteGallery.vue
+++ src/components/QuoteGallery.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, onMounted, onUnmounted, nextTick } from "vue";
+import { ref, onMounted, onUnmounted, nextTick, watch } from "vue";
 import lozad from "lozad";
 
 import { Quote } from "../types.d";
@@ -7,7 +7,8 @@ import { chunk } from "../utils/helpers";
 import quotesRaw from "../assets/quotes.json"
 
 let observer: lozad.Observer;
-const quotesChunked = chunk(quotesRaw, 8);
+let allQuotes = ref(quotesRaw)
+let quotesChunked = chunk(allQuotes.value, 8);
 
 const quotes = ref<Quote[]>(quotesChunked[0]);
 const quotesIndex = ref(0);
@@ -15,6 +16,40 @@ const galleryElement = ref<HTMLDivElement>();
 
 const isShowDialog = ref(false);
 const selectedQuote = ref<Quote>();
+
+const search = ref("")
+const showFilter = ref(false)
+const searchFilter = ref(["quotes", "from", "user"]);
+const chooseFilter = ref(searchFilter.value[0])
+
+function filterQuotes(valueSearch: string, type: string){
+  let arr = allQuotes.value.filter((e) =>{
+    if(type == "quotes"){
+      return valueSearch.toLowerCase().split(' ').every(v => e.text.toLowerCase().includes(valueSearch))
+    }
+    else if(type == "from"){
+      return valueSearch.toLowerCase().split(' ').every(v => e.author.toLowerCase().includes(valueSearch))
+    }
+    else if(type == "user"){
+      return valueSearch.toLowerCase().split(' ').every(v => e.github.name.toLowerCase().includes(valueSearch))
+    }
+  })
+  return arr
+}
+
+watch(search, (newValue) => {
+  let arr = filterQuotes(newValue, chooseFilter.value)
+  quotesChunked = chunk(arr, 8)
+  quotes.value = quotesChunked[0]
+  quotesIndex.value = 0
+})
+
+watch(chooseFilter, (newValue) => {
+  let arr = filterQuotes(search.value, newValue)
+  quotesChunked = chunk(arr, 8)
+  quotes.value = quotesChunked[0]
+  quotesIndex.value = 0
+})
 
 function displayDialog(quote: Quote) {
   isShowDialog.value = true;
@@ -74,6 +109,25 @@ onUnmounted(function () {
 
 <template>
   <Dialog v-if="selectedQuote" :quote="selectedQuote" :show="isShowDialog" @close="closeDialog" />
+  <!-- search box -->
+  <div class="flex flex-col items-center justify-center">
+    <div class="w-11/12 md:w-3/4 mb-[86px] bg-gradient-to-r from-purple-500 via-pink-400 to-red-400 rounded-lg relative">
+      <div class="flex justify-between items-center p-1">
+        <button @focus="showFilter = true" @blur="showFilter = false" @mousedown="showFilter == true? showFilter = false : showFilter = true" @click.prevent class="relative appearance-none bg-gradient-to-r from-purple-600 to-red-400 rounded-l-md py-2 px-3 h-full flex justify-between items-center">
+          <span class="text-white flex justify-between items-center capitalize">{{ chooseFilter }}</span>
+          <span class="text-white ml-4 cursor-pointer">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-down" viewBox="0 0 16 16">
+              <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
+            </svg>
+          </span>
+          <div class="absolute left-0 right-0 top-full z-10 rounded-lg bg-gradient-to-r from-purple-500 via-pink-400 to-red-400 transition-all" :class="showFilter? 'opacity-100' : 'opacity-0'">
+            <span v-for="(filter, index) in searchFilter" :key="index + 'filter'" class="capitalize text-white block px-3 py-1 hover:font-bold" @click="chooseFilter = filter">{{ filter }}</span>
+          </div>
+        </button>
+        <input type="text" v-model="search" placeholder="Search" class="py-2 px-3 rounded-r-md w-full transition appearance-none outline-none font-semibold">
+      </div>
+    </div>
+  </div>
 
   <div ref="galleryElement" class="flex flex-col items-center justify-center">
     <div class="w-11/12 md:w-3/4 mb-[86px]">
