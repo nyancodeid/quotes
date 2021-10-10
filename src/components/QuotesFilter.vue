75f819715c14cc7a7097d1a456c98b6a35028f48
nyancodeid <ryandevstudio@gmail.com>
2021-10-10 18:36:40 +0700

    refactor: extract class to style @apply

--- src/components/QuotesFilter.vue
+++ src/components/QuotesFilter.vue
@@ -26,10 +26,10 @@ function changeFilterHandler (option: string) {
 </script>
 
 <template>
-  <div class="relative inline-block text-left">
-    <div class="w-[106px]">
-      <button type="button" class="inline-flex justify-between w-full rounded-l-md border border-gray-300 dark:border-gray-500 shadow-sm px-4 py-2 bg-gray-100 dark:bg-gray-700 text-sm font-medium capitalize text-gray-700 dark:text-white hover:bg-gray-50 dark:hover:border-gray-200 transition-colors duration-300 focus:outline-none" id="menu-button" aria-expanded="true" aria-haspopup="true" @click="filterOpen = !filterOpen">
-        {{ (selectedFilter === "") ? "Filter" : selectedFilter }}
+  <div class="filter-container">
+    <div class="filter-button--container">
+      <button type="button" class="filter-button" id="menu-button" aria-expanded="true" aria-haspopup="true" @click="filterOpen = !filterOpen">
+        {{ selectedFilter }}
         <i-heroicons-solid-chevron-down class="-mr-1 ml-2 h-5 w-5" />
       </button>
     </div>
@@ -42,11 +42,40 @@ function changeFilterHandler (option: string) {
       leave-from-class="transform opacity-100 scale-100"
       leave-to-class="transform opacity-0 scale-95"
     >
-      <div ref="filterPopupElement" class="origin-top-left absolute z-10 left-0 mt-2 w-56 rounded-md shadow-lg bg-white dark:bg-gray-900 ring-1 ring-black dark:ring-gray-500 ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1" v-if="filterOpen">
+      <div ref="filterPopupElement" class="filter-popup--container" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1" v-if="filterOpen">
         <div class="py-1" role="none">
-          <button class="block w-full text-left px-4 py-2 text-sm capitalize" role="menuitem" tabindex="-1" v-for="option in filterOptions" :key="option" :class="(option === selectedFilter) ? `bg-gray-100 text-gray-900 dark:bg-gray-800 dark:text-gray-200` : `text-gray-700 dark:text-gray-300`" @click="changeFilterHandler(option)">{{ option }}</button>
+          <button 
+            class="filter-popup--menu-item" 
+            role="menuitem"
+            tabindex="-1"
+            v-for="option in filterOptions" 
+            :key="option" 
+            :class="{ active: (option === selectedFilter) }"
+            @click="changeFilterHandler(option)"
+          >{{ option }}</button>
         </div>
       </div>
     </transition>
   </div>
 </template>
+
+<style>
+.filter-container {
+  @apply relative inline-block text-left;
+}
+.filter-button--container {
+  @apply w-[106px];
+}
+.filter-button--container button.filter-button {
+  @apply inline-flex justify-between w-full rounded-l-md border border-gray-300 dark:border-gray-500 shadow-sm px-4 py-2 bg-gray-100 dark:bg-gray-700 text-sm font-medium capitalize text-gray-700 dark:text-white hover:bg-gray-50 dark:hover:border-gray-200 transition-colors duration-300 focus:outline-none;
+}
+.filter-popup--container {
+  @apply origin-top-left absolute z-10 left-0 mt-2 w-56 rounded-md shadow-lg bg-white dark:bg-gray-900 ring-1 ring-black dark:ring-gray-500 ring-opacity-5 focus:outline-none;
+}
+.filter-popup--container button.filter-popup--menu-item {
+  @apply block w-full text-left px-4 py-2 text-sm capitalize text-gray-700 dark:text-gray-300;
+}
+.filter-popup--container button.filter-popup--menu-item.active {
+  @apply bg-gray-100 dark:bg-gray-800 text-gray-900 dark:text-gray-200;
+}
+</style>
