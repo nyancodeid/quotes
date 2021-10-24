a04e56a5167fb9dfc58ee2371f4dafcc95557f28
Ryan Aunur <ryandevstudio@gmail.com>
2021-10-24 18:43:02 +0700

    update(app-hero): width and show badge on gallery only

--- src/components/AppHero.vue
+++ src/components/AppHero.vue
@@ -40,11 +40,11 @@ onMounted(() => {
     <h1 class="mt-4 md:mt-0 text-6xl font-bold mb-4 text-transparent bg-clip-text bg-gradient-to-br from-pink-400 to-purple-600">
       #Quote's
     </h1>
-    <p class="w-11/12 md:w-1/2 mb-4 text-sm md:text-xl text-center text-gray-500 dark:text-gray-50">
+    <p class="w-11/12 md:w-3/4 mb-4 text-sm md:text-xl text-center text-gray-500 dark:text-gray-50">
       Kumpulan quote oleh orang-orang hebat di dunia sebagai penasehat dan penyemangat. Dibuat untuk event Hacktoberfest 2021.
     </p>
 
-    <div class="w-11/12 md:w-1/2 flex justify-center mb-4">
+    <div class="w-11/12 md:w-3/4 flex justify-center mb-4" :class="{ 'hidden': (route.name !== 'quote-gallery') }">
       <a class="mr-1" href="https://github.com/nyancodeid/quotes">
         <img src="https://img.shields.io/badge/github-quotes-brightgreen?logo=github&style=flat" width="109" height="20" alt="Repository">
       </a>
