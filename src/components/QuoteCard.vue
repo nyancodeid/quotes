98d49326e289388c93249b21dfc4cc488d76ac3b
Ryan Aunur <ryandevstudio@gmail.com>
2021-10-24 18:45:52 +0700

    feat(quote-card): card size xl and open link

--- src/components/QuoteCard.vue
+++ src/components/QuoteCard.vue
@@ -25,13 +25,20 @@ const isFavorited = computed(() => isFavorite(quote.value.id))
 function getGradientByIndex(index = 0) {
   return gradients[index]
 }
+function openQuotePage(slug: string) {
+  window.open(`quote/${slug}`, '_blank')
+}
 </script>
 
 <template>
   <div ref="card" class="w-full relative text-white overflow-hidden flex rounded-3xl shadow-lg p-2" :class="`card-size--${size} ` + getGradientByIndex(quote.gradient_id)">
     <div class="w-full flex flex-col dark:bg-gray-800 dark:rounded-2.2xl">
       <div class="card-content flex flex-col items-start relative p-6 xl:p-8">
-        <h2 class="card-content--icon">
+        <div v-if="(size === 'sm')" class="absolute top-4 right-6 p-2 rounded-full hover:bg-black hover:bg-opacity-25 dark:hover:bg-gray-600 transition-colors hide-on-save button-save cursor-pointer" @click.stop="openQuotePage(quote.slug)">
+          <i-mdi-open-in-new />
+        </div>
+
+        <h2 class="card-content--icon flex-1">
           <i-ri-double-quotes-l />
         </h2>
         <p class="card-content--text">
@@ -40,7 +47,7 @@ function getGradientByIndex(index = 0) {
         <p v-if="isValidLink(quote.author_detail_url)" class="card-content--author text-sm text-gray-100">
           - <a :href="quote.author_detail_url" target="_blank" :title="`Read more about ${quote.author}`" rel="noreferrer noopener" class="pb-1 hover:border-b-2 hover:border-gray-200">{{ quote.author }}</a>
         </p>
-        <p v-else class="card-content--author text-sm text-gray-100">
+        <p v-else class="card-content--author text-sm md:whitespace-nowrap text-gray-100">
           - {{ quote.author }}
         </p>
       </div>
@@ -68,8 +75,8 @@ function getGradientByIndex(index = 0) {
             </div>
 
             <div class="flex items-center">
-              <span class="text-xs mr-2 text-white text-opacity-80">
-                <span class="font-bold text-white">{{ quote.github?.followers }}</span> followers
+              <span class="text-xs mr-2 text-white text-opacity-80 inline-flex">
+                <span class="font-bold text-white mr-1">{{ quote.github?.followers }}</span> followers
               </span>
             </div>
           </div>
@@ -80,7 +87,7 @@ function getGradientByIndex(index = 0) {
               <i-ri-heart-2-line v-else />
             </div>
 
-            <div v-if="(size === 'lg')" class="p-2 rounded-full hover:bg-black hover:bg-opacity-25 dark:hover:bg-gray-600 transition-colors hide-on-save button-save cursor-pointer" title="Simpan quote menjadi gambar" @click.stop="saveCard">
+            <div v-if="(size === 'lg' || size === 'xl')" class="p-2 rounded-full hover:bg-black hover:bg-opacity-25 dark:hover:bg-gray-600 transition-colors hide-on-save button-save cursor-pointer" title="Simpan quote menjadi gambar" @click.stop="saveCard">
               <i-ri-save-line />
             </div>
           </div>
@@ -112,9 +119,9 @@ function getGradientByIndex(index = 0) {
 }
 .card-size--xl .card-content .card-content--icon,
 .card-size--xl .card-content .card-content--text {
-  @apply text-3xl leading-relaxed;
+  @apply text-xl md:text-[1.875em] leading-relaxed;
 }
 .card-size--xl .card-content .card-content--author {
-  @apply text-xl;
+  @apply text-base md:text-xl;
 }
 </style>
