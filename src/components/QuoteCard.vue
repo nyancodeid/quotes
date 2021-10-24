3d367f17f833b17d26d4b039ba9625d8b4abf168
Ryan Aunur <ryandevstudio@gmail.com>
2021-10-24 22:52:26 +0700

    refactor: use link element instead of div element

--- src/components/QuoteCard.vue
+++ src/components/QuoteCard.vue
@@ -25,8 +25,10 @@ const isFavorited = computed(() => isFavorite(quote.value.id))
 function getGradientByIndex(index = 0) {
   return gradients[index]
 }
-function openQuotePage(slug: string) {
-  window.open(`quote/${slug}`, '_blank')
+function openQuotePage(event: Event) {
+  const element = (event.currentTarget as HTMLLinkElement)
+
+  window.open(element.href, '_blank')
 }
 </script>
 
@@ -34,9 +36,9 @@ function openQuotePage(slug: string) {
   <div ref="card" class="w-full relative text-white overflow-hidden flex rounded-3xl shadow-lg p-2" :class="`card-size--${size} ` + getGradientByIndex(quote.gradient_id)">
     <div class="w-full flex flex-col dark:bg-gray-800 dark:rounded-2.2xl">
       <div class="card-content flex flex-col items-start relative p-6 xl:p-8">
-        <div v-if="(size === 'sm')" class="absolute top-4 right-6 p-2 rounded-full hover:bg-black hover:bg-opacity-25 dark:hover:bg-gray-600 transition-colors hide-on-save button-save cursor-pointer" @click.stop="openQuotePage(quote.slug)">
+        <a v-if="(size === 'sm')" :href="`/quote/${quote.slug}`" target="_blank" class="absolute top-4 right-6 p-2 rounded-full hover:bg-black hover:bg-opacity-25 dark:hover:bg-gray-600 transition-colors hide-on-save button-save cursor-pointer" @click.stop="openQuotePage">
           <i-mdi-open-in-new />
-        </div>
+        </a>
 
         <h2 class="card-content--icon flex-1">
           <i-ri-double-quotes-l />
